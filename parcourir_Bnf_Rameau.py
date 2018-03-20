260a340c7f65b337e3e742d5a6d17a47e32d8e11
DJERROUD Ilyes <37126453+ilyesDjerroud@users.noreply.github.com>
2018-03-20 17:23:49 +0100

    Add files via upload

--- parcourir_Bnf_Rameau.py
+++ parcourir_Bnf_Rameau.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-import codecs
+
 import re, sys
 
 
 
 #//////////////////// on ouvre le fichier BNF , lecture ligne par ligne
-file1 = codecs.open('BNF_utf-8.txt','r','utf-8')
-read1= file1.readlines()
+file1 =open('BNF_utf-8.txt','r')
+read1= file1.read()
 
 
 
 #/////////////////// on ouvre le fichier RAMEAU , lecture ligne par ligne
-file2 = codecs.open('RAMEAU_utf-8.txt','r')
-read2 = file2.readlines()
+file2 = open('RAMEAU_utf-8.txt','r')
+read2 = file2.read()
 
 
 
@@ -22,26 +22,46 @@ read2 = file2.readlines()
 #////////////////// création d'un fichier pour aligner les termes des deux fichiers
 nFichierBNF = open('newBNF_utf-8.txt','x+')
 nFichierRM = open('newRAMEAU_utf-8.txt','x+')
-nAlignement = open('AlignementBNF_RAMEAU.txt','x+')
+#nAlignement = open('AlignementBNF_RAMEAU.txt','x+')
 
 
-#//////////////// on parcour le fichier BNF , on récupère dans tout les lignes que la partie aprés le $m , on remplace les $e et $i par des "-"
+#//////////////// on parcour le fichier BNF , on récupère dans tout les lignes que la partie aprés le dernier $ 
 #/////////////// on écrit ces lignes là dans le nouveau fichier BNF
 
+res1 = re.findall(r"(\d{8})\n\d{3}\s+\$.*\$[a-z](.*)",read1)
+if res1:
+	for ligne in res1:	
+		#print(ligne,'\n')
+		
+		nFichierBNF.write(str(ligne)+'\n')
+
+
+
+
+
+
+""" #temchi
 for x in read1:
-	termes = re.search(r"\d{3}\s+\$a.*\$m(.*)",x)
+	identifiant = re.search(r"(\d{8})",x)
+
+	termes1 = re.search(r"\d{3}\s+\$.*\$[a-z](.*)",x)
+
 	#identifiants = re.search(r"(\d{8})",ligne)
 	#if identifiants:
-	if termes:	
 
-		termeBNF = termes.group(1) 
-		termeBNF2 = termeBNF.replace("$e"," ")
-		termeBNF3 = termeBNF2.replace("$i"," ")
-		print(termeBNF3)
+	if termes1:
+		termeBNF = termes1.group(1)
+		#print(termeBNF)
 
-		nFichierBNF.write("\n"+termeBNF3)
+	if identifiant :
+		identifiant1 = identifiant.group(1)
+
+print(termeBNF,":",identifiant1)
+		
+
 	
-
+"""
+		#nFichierBNF.write(identifiant1 +" : "+termeBNF3)
 
 
 
@@ -51,47 +71,62 @@ for x in read1:
 #/////////////// on écrit ces lignes là dans le nouveau fichier RAMEAU
 
 
+res2 = re.findall(r"(\d{8})\s+\$a(.+)\$",read2)
+if res2:
+	for ligne in res2:	
+		#print(ligne,'\n')
+		
+		nFichierRM.write(str(ligne)+'\n')
+
+
+
+
+
+
+
+
+"""
 for y in read2 :
-	lignes = re.search(r"(\d{8})\s+\$a(.+)", y)
+	lignes = re.search(r"(\d{8})\s+\$a(.+)\$", y)
 
 	if lignes:
 
-		identifiant = lignes.group(1)
+		identifiant2 = lignes.group(1)
 
 	
 		termeRM=lignes.group(2)
-		termeRM2=termeRM.replace("$g"," ")
 		
-		print(termeRM2)
+		
+		print(identifiant+termeRM)
 
-		nFichierRM.write("\n"+termeRM2)
+		nFichierRM.write("\n"+termeRM)
 		
 
-		nAlignement.write("\n"+termeBNF3+"       "+termeRM2)
+		#nAlignement.write("\n"+termeBNF3+"       "+termeRM2)
 
-
-def levenshtein(mot1,mot2):
+"""
+#def levenshtein(mot1,mot2):
 	
-	ligne_i = [ k for k in range(len(mot1)+1) ]
+#	ligne_i = [ k for k in range(len(mot1)+1) ]
 
-	for i in range(1, len(mot2) + 1):
+#	for i in range(1, len(mot2) + 1):
 
-		ligne_prec = ligne_i
+#		ligne_prec = ligne_i
 		
-		ligne_i = [i]*(len(mot1)+1)
+#		ligne_i = [i]*(len(mot1)+1)
 
-		for k in range(1,len(ligne_i)):
+#		for k in range(1,len(ligne_i)):
 			
-			cout = int(mot1[k-1] != mot2[i-1])
+#			cout = int(mot1[k-1] != mot2[i-1])
 			
-			ligne_i[k] = min(ligne_i[k-1] + 1, ligne_prec[k] + 1, ligne_prec[k-1] + cout)
+#			ligne_i[k] = min(ligne_i[k-1] + 1, ligne_prec[k] + 1, ligne_prec[k-1] + cout)
 			
-	return ligne_i[len(mot1)]
+#	return ligne_i[len(mot1)]
 	
 
 
 
-print (levenshtein(termeBNF3, termeRM2))
+#print (levenshtein(termeBNF3, termeRM2))
 
 
 
